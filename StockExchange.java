9de6e6d6aad80ed051be1b3f6eff243aad2c89f8
amychou <amychou124@gmail.com>
2014-09-24 12:38:52 -0400

    StockExchange completed

--- StockExchange.java
+++ StockExchange.java
@@ -1,7 +1,29 @@
 /**
- * @author Amy Chou, Aneesh Ashutosh, Cam Wong, Seho Young
+ * @author Amy Chou
  */
 
-public class StockExchange {
+import java.util.*;
 
+public class StockExchange {
+  
+  private Map<String, Stock> stocks;
+  
+  public StockExchange()
+  {
+    stocks = new HashMap<String, Stock>();
+  }
+  
+  public void listStock(String symbol, String name, double price)
+  {
+    stocks.put(symbol, new Stock(symbol, name, price));
+  }
+  
+  public String getQuote(String symbol) { return stocks.get(symbol).getQuote(); }
+  
+  public void placeOrder(TradeOrder order)
+  {
+    stocks.get(order.getSymbol().placeOrder(order));
+  }
+  
+  
 }

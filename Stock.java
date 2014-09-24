21303d002bff05dab9c809d7182b32c8aaed8d0a
gnuoyohes <sehoyoung@sehos-notebook.andover.edu>
2014-09-24 13:01:23 -0400

    dffbebe

--- Stock.java
+++ Stock.java
@@ -1,7 +1,18 @@
 /**
  * @author Amy Chou, Aneesh Ashutosh, Cam Wong, Seho Young
+ * @date 10/01/14
  */
 
-public class Stock {
-
+public class Stock
+{
+	private String symbol;
+	private String name;
+	private double price;
+	
+	public Stock(String symbol, String name, double price)
+	{
+		this.symbol = symbol;
+		this.name = name;
+		this.price = price;
+	}
 }
--- StockExchange.java
+++ StockExchange.java
@@ -1,7 +1,34 @@
 /**
+<<<<<<< HEAD
+ * @author Amy Chou
+=======
  * @author Amy Chou, Aneesh Ashutosh, Cam Wong, Seho Young
+ * @date 10/01/14
+>>>>>>> FETCH_HEAD
  */
 
-public class StockExchange {
+import java.util.*;
 
+public class StockExchange {
+  
+  private Map<String, Stock> stocks;
+  
+  public StockExchange()
+  {
+    stocks = new HashMap<String, Stock>();
+  }
+  
+  public void listStock(String symbol, String name, double price)
+  {
+    stocks.put(symbol, new Stock(symbol, name, price));
+  }
+  
+  public String getQuote(String symbol) { return stocks.get(symbol).getQuote(); }
+  
+  public void placeOrder(TradeOrder order)
+  {
+    stocks.get(order.getSymbol().placeOrder(order));
+  }
+  
+  
 }

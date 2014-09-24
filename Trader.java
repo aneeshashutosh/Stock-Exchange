ce17b5d5cec2e93ee6955b87fe91f9bfef79a4ea
gnuoyohes <sehoyoung@sehos-notebook.andover.edu>
2014-09-24 12:36:18 -0400

    Trader
    added some methods

--- Trader.java
+++ Trader.java
@@ -2,6 +2,21 @@
  * @author Amy Chou, Aneesh Ashutosh, Cam Wong, Seho Young
  */
 
-public class Trader {
-
+public class Trader implements Comparable<Trader>
+{
+  private Brokerage brokerage;
+  private String name;
+  private String pswd;
+  
+  public Trader(Brokerage brokerage, String name, String pswd)
+  {
+    this.brokerage = brokerage;
+    this.name = name;
+    this.pswd = pswd;
+  }
+  
+  public int compareTo(Trader other)
+  {
+    return name.compareToIgnoreCase(other.name);
+  }
 }

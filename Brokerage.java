9275c4abfa42ca4b4dc1525144196f7b03fefe33
aneeshashutosh <aneeshashutosh+github@gmail.com>
2014-09-24 12:57:48 -0400

    fixed head things maybe?

--- Brokerage.java
+++ Brokerage.java
@@ -1,15 +1,17 @@
+import java.util.TreeMap;
+import java.util.TreeSet;
+
 /**
  * @author Amy Chou, Aneesh Ashutosh, Cam Wong, Seho Young
  * @date 10/01/14
  */
 
-<<<<<<< HEAD
 public class Brokerage
-	implements Login {
+implements Login {
 	private final StockExchange exchange;
 	private TreeMap<String, Trader> registeredUsers;
 	private TreeSet<Trader> activeUsers;
-	
+
 	@Override
 	public int addUser(String name, String pswd) {
 		if (this.registeredUsers.containsKey(name)) {
@@ -27,7 +29,7 @@ public class Brokerage
 		this.registeredUsers.put(name, new Trader(this, name, pswd));
 		return 0;
 	}
-	
+
 	@Override
 	public int login(String name, String pswd) {
 		if (!this.registeredUsers.containsKey(name)) {
@@ -43,26 +45,22 @@ public class Brokerage
 		this.activeUsers.add(this.registeredUsers.get(name));
 		return 0;
 	}
-	
+
 	public void getQuote(String symb, Trader tr) {
 		tr.receiveMessage(this.exchange.getQuote(symb));
 	}
-	
+
 	public void logout(Trader tr) {
 		this.activeUsers.remove(tr);
 	}
-	
+
 	public void placeOrder(TradeOrder order) {
 		this.exchange.placeOrder(order);
 	}
-	
+
 	public Brokerage(StockExchange exchange) {
 		this.exchange = exchange;
 		this.registeredUsers = new TreeMap<>();
 		this.activeUsers = new TreeSet<>();
 	}
-=======
-public class Brokerage {
-
->>>>>>> parent of 258b852... Implement Login methods for Brokerage
 }

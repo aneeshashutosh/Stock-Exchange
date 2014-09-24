e626168df7fdaeef38efaeb2fb09a7b9903ce61a
gnuoyohes <sehoyoung@sehos-notebook.andover.edu>
2014-09-24 13:00:20 -0400

    Trader
    Finished

--- Trader.java
+++ Trader.java
@@ -1,12 +1,16 @@
 /**
  * @author Amy Chou, Aneesh Ashutosh, Cam Wong, Seho Young
+ * @date 10/01/14
  */
+import java.util.*;
 
 public class Trader implements Comparable<Trader>
 {
   private Brokerage brokerage;
   private String name;
   private String pswd;
+  private TraderWindow myWindow;
+  private Queue<String> mailbox;
   
   public Trader(Brokerage brokerage, String name, String pswd)
   {
@@ -15,8 +19,55 @@ public class Trader implements Comparable<Trader>
     this.pswd = pswd;
   }
   
+  public String getName()
+  {
+    return name;
+  }
+  
+  public String getPassword()
+  {
+    return pswd;
+  }
+  
   public int compareTo(Trader other)
   {
     return name.compareToIgnoreCase(other.name);
+  }
+<<<<<<< HEAD
+  
+  public boolean equals(Trader other)
+  {
+    return name.equalsIgnoreCase(other.name);
+  }
+  
+  public void openWindow()
+  {
+    myWindow = new TraderWindow(this);
+  }
+  
+  public boolean hasMessages()
+  {
+    return !mailbox.isEmpty();
+  }
+  
+  public void receiveMessage(String msg)
+  {
+    mailbox.add(msg);
+  }
+  
+  public void getQuote(String symbol)
+  {
+    brokerage.getQuote(symbol, this);
+  }
+  
+  public void placeOrder(TradeOrder order)
+  {
+    brokerage.placeOrder(order);
+  }
+  
+  public void quit()
+  {
+    brokerage.logout(this);
+    myWindow = null;
   }
 }

3bab93e0d9cbcf593021cd1050125cf378facf9d
aneeshashutosh <aneeshashutosh+github@gmail.com>
2014-09-24 12:40:50 -0400

    Fixed some formatting errors
    OCD strikes again

--- TradeOrder.java
+++ TradeOrder.java
@@ -5,36 +5,36 @@
 
 public class TradeOrder
 {
-	  private Trader trader;
-	  private String symbol;
-	  private boolean buyOrder;
-	  private boolean marketOrder;
-	  private int numShares;
-	  private double price;
+	private Trader trader;
+	private String symbol;
+	private boolean buyOrder;
+	private boolean marketOrder;
+	private int numShares;
+	private double price;
 
-	  public TradeOrder(Trader trader, String symbol, boolean buyOrder, boolean marketOrder, int numShares, double price)
-	  {
-	    this.trader = trader;
-	    this.symbol = symbol;
-	    this.buyOrder = buyOrder;
-	    this.marketOrder = marketOrder;
-	    this.numShares = numShares;
-	    this.price = price;
-	  }
-	  
-	  public void subtractShares(int shares)
-	  {
-	    if (this.numShares < shares)
-	      throw new IllegalArgumentException();
-	    this.numShares -= shares;
-	  }
+	public TradeOrder(Trader trader, String symbol, boolean buyOrder, boolean marketOrder, int numShares, double price)
+	{
+		this.trader = trader;
+		this.symbol = symbol;
+		this.buyOrder = buyOrder;
+		this.marketOrder = marketOrder;
+		this.numShares = numShares;
+		this.price = price;
+	}
 
-	  public Trader getTrader() { return this.trader; }
-	  public String getSymbol() { return this.symbol; }
-	  public boolean isBuy() { return this.buyOrder; }
-	  public boolean isSell() { return !this.buyOrder; }
-	  public boolean isMarket() { return this.marketOrder; }
-	  public boolean isLimit() { return !this.marketOrder; }
-	  public int getShares() { return this.numShares; }
-	  public double getPrice() { return this.price; }
+	public void subtractShares(int shares)
+	{
+		if (this.numShares < shares)
+			throw new IllegalArgumentException();
+		this.numShares -= shares;
+	}
+
+	public Trader getTrader() { return this.trader; }
+	public String getSymbol() { return this.symbol; }
+	public boolean isBuy() { return this.buyOrder; }
+	public boolean isSell() { return !this.buyOrder; }
+	public boolean isMarket() { return this.marketOrder; }
+	public boolean isLimit() { return !this.marketOrder; }
+	public int getShares() { return this.numShares; }
+	public double getPrice() { return this.price; }
 }
